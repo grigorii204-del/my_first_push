646bb1ea4c21017ebe04a2f904aaf6511b32a5d7
grigorii204-de <grigorii204@gmail.com>
2026-06-10 20:28:36 +0300

    My new code

---
+++ test.py
@@ -0,0 +1,10 @@
+class Program:
+    def __init__(self, name):
+        name = name
+    
+    def printf(self):
+        print(f"Your name: {self.name}")
+        
+        
+prog = Program("Alexandr")
+prog.printf()
